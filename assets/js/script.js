d4b9edf39bd3879dbace99467ff47a08bb350a6e
David Sha <70577814+d4vidsha@users.noreply.github.com>
2023-01-10 16:43:04 +1100

    prevent form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,7 +135,10 @@ for (let i = 0; i < formInputs.length; i++) {
 }
 
 // add event to form submit button
-formBtn.addEventListener("click", function () {
+formBtn.addEventListener("click", function (event) {
+
+  // prevent form submit
+  event.preventDefault();
 
   // show alert message
   alert("Form submitted successfully!");
