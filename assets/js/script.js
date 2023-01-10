8fc9c4a17d8fc13750f4310ee4373996faeb8eb7
David Sha <70577814+d4vidsha@users.noreply.github.com>
2023-01-11 08:40:54 +1100

    include in-tab method

--- assets/js/script.js
+++ assets/js/script.js
@@ -173,6 +173,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
     // check if the link is the "Blog" link
     if(this.innerHTML.toLowerCase() === "blog"){
         event.preventDefault();
+        // window.location.href = "https://blog.davidsha.me";
         window.open("https://blog.davidsha.me", "_blank");
         return;
     }
